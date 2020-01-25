53e319cc685557cd67f7f972a46dd9d1542a6c1a
Michael King <michael.ac.king@outlook.com>
2020-01-25 12:03:17 -0500

    Add introductory message to the BullCowCartridge

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,8 +4,23 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bulls & Cows!"));
+    PrintLine(TEXT("Please enter your name:"));
 }
 
 void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
+    ClearScreen();
+    FString HiddenWord = TEXT("dog");
+    PrintLine(Input);
 }
+
+
+
+/* GAME LOOP
+    1. Welcome the user
+    2. Set the HiddenWord
+    3. Take a guess
+    4. Check if the guess is correct
+    5. Win or Lose
+*/
