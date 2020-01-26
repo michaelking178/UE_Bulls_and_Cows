d9ba946e751c50ae42745bede2df88a736ab4dd9
Michael King <michael.ac.king@outlook.com>
2020-01-26 10:54:10 -0500

    Implement basic player input

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # BullCowGame
 
-Project from the GameDev.tv course "Unreal 4.22 C++ Developer: Learn C++ and Make Video Games."
+A basic word guessing game from the GameDev.tv course "Unreal 4.22 C++ Developer: Learn C++ and Make Video Games."
 
 Developed with Unreal Engine 4.22 using C++.
--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,22 +5,37 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bulls & Cows!"));
-    PrintLine(TEXT("Please enter your name:"));
+    PrintLine(TEXT("Please guess the secret isogram:"));
+    HiddenWord = TEXT("dog");
 }
 
 void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("dog");
-    PrintLine(Input);
+    PrintLine(TEXT("You guessed: " + Input));
+
+    if (Input == HiddenWord) {
+        PrintLine(TEXT("You are correct! YOU WIN!"));
+    }
+    else {
+        PrintLine(TEXT("You are wrong. YOU LOSE."));
+    }
 }
 
 
 
 /* GAME LOOP
     1. Welcome the user
-    2. Set the HiddenWord
+    2. Set up the game
+        a. Set the HiddenWord
+        b. Set the player lives
     3. Take a guess
     4. Check if the guess is correct
-    5. Win or Lose
+        a. Correct
+            i. Deliver WIN message
+        b. Incorrect
+            i. Deliver INCORRECT message
+            ii. Deprecate player lives
+            iii. If lives run out, deliver LOSE message
+    5. Restart or Quit
 */
